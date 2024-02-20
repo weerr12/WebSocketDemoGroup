34fc5b44e25b0d541f5af88687eb82efa1f2c611
werasak <jokerwwe0542@gmail.com>
2024-02-20 20:12:38 +0700

    lab17commit

--- SpringSocketDemo/src/main/java/com/websocket/demo/config/WebSocketConfig.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/config/WebSocketConfig.java
@@ -5,30 +5,19 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    /*
-    * defined connection endpoint
-    * */
+public class WebSocketConfig implements  WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
-
-
-    /**
-     * defined message passing endpoint
-     * @param registry
-     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry
-                .setApplicationDestinationPrefixes("/app")
+        registry.setApplicationDestinationPrefixes("/app")
                 .enableSimpleBroker("/topic");
     }
 }
